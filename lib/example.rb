565ceedbc23a3bae3ec98092b778cbc18cd70717
Amanda Mongiove <amandamongiove@gmail.com>
2020-08-28 23:09:38 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,4 @@
-# Use this space to code along with the readme
+def say_hello_world_ten_times# Use this space to code along with the readme
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -10,3 +10,6 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
+
+say_hello_world_ten_times
